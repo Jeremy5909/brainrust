2814c2e76ce868e6473d45d377dd6d7c5c27ee3c
Jeremy5909 <jeremyherczeg@gmail.com>
2025-06-03 13:05:03 -0700

    Move repo to be based in lib

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use brainrust::{Instruction, Program};
-
-fn main() {
-    let prog = Program::new()
-        .add_instruction(Instruction::AddString("x".to_owned(), "Hello!".to_owned()))
-        .debug()
-        .build();
-    println!("{prog}");
-}
